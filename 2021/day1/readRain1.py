5653b1f29f4a548d8ca3e9250794981c25e58db1
Assela Pathirana <assela@pathirana.net>
2021-10-04 20:56:44 +0200

    Day 1 programes

---
+++ 2021/day1/readRain1.py
@@ -0,0 +1,21 @@
+with open('data/funabashi_rainfall.txt') as f:
+    count = 0
+    sum = 0
+    maxv = 0
+    
+    while True:
+        if count%24 == 0:
+            #print(sum)
+            if sum > maxv:
+                maxv = sum
+            sum = 0
+        count += 1
+            
+        line = f.readline()
+        if not line:
+            break
+        val = line.split()[-1]
+        sum += int(val)
+        # print(count, val)
+
+    print("Maximum 24h rainfall value ", maxv)
