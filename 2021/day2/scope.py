b0fb37c75def5a9b742f2855923dd8f046e17e7a
Assela Pathirana <assela@pathirana.net>
2021-10-05 10:56:34 +0200

    EPANET files

---
+++ 2021/day2/scope.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct  5 10:16:06 2021
+
+@author: apa
+"""
+
+
+for item in [1, 2, 3]:
+    print("Item:", item)
+    sq=item**2
+print(sq)
+
+print("done")
+
+    
