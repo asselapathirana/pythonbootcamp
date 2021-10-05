93385a2b5a24dc429f1da98380dec7e9f78e1b14
Assela Pathirana <assela@pathirana.net>
2021-10-05 10:07:07 +0200

    functions intro

---
+++ 2021/day2/prime_dev7.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct  5 09:40:28 2021
+
+@author: apa
+"""
+
+import numUtils as nu
+
+
+ct=1
+while True:
+    if nu.isPrime(ct):
+        print(ct)
+    ct+=1
+    
