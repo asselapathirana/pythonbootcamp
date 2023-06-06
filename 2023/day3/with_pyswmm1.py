29f5fabc6ec490c1e43a0c58e01df00262fe7793
Assela Pathirana <assela@pathirana.net>
2023-06-06 11:02:03 +0200

    fixed code

--- 2023/day3/with_pyswmm1.py
+++ 2023/day3/with_pyswmm1.py
@@ -1,12 +1,12 @@
 from swmm.toolkit.shared_enum import NodeAttribute
 import pyswmm as ps
-sim = ps.Simulation('./swmm5Example.inp')
+sim = ps.Simulation('./data/swmm5Example.inp')
 sim.execute()
 
-with ps.Simulation('./swmm5Example.inp') as sim:
+with ps.Simulation('./data/swmm5Example.inp') as sim:
     nodes_object = ps.Nodes(sim)
     print([x.nodeid for x in list(nodes_object)])
-with ps.Output('./swmm5Example.out') as out:
+with ps.Output('./data/swmm5Example.out') as out:
     print (out.nodes)
     ts = out.node_series('J1', NodeAttribute.INVERT_DEPTH, 0, 55 )
     for index in ts:
